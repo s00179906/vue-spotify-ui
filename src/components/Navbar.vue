db56a4c0fda3682a0b5dcbd8a9ca9bea4e902d55
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-12-11 17:42:10 +0000

    Navbar.vue: changed custom css to vuetify css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,53 +1,54 @@
 <template>
   <div class="navbar">
-    <div class="navbar-items">
-      <div class="spotify-logo">
-        <img src="@/assets/Spotify_logo.svg" />
-      </div>
-
-      <div class="navbar-links">
-        <ul>
-          <li>
-            <svg width="5" height="30">
-              <rect width="300" height="100" style="fill:#1ed760;" />
-            </svg>
-            <button>
+    <v-card class="mx-auto" color="black" tile height="100%">
+      <v-list shaped color="black">
+        <v-subheader
+          ><div class="spotify-logo mt-2">
+            <v-img src="@/assets/Spotify_logo.svg" width="140" />
+          </div>
+        </v-subheader>
+        <v-list-item-group class="mt-5">
+          <v-list-item dark>
+            <v-list-item-icon>
               <img src="@/assets/Spotify_home.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
               Home
-            </button>
-          </li>
-          <li>
-            <svg width="5" height="30">
-              <rect width="300" height="100" style="fill:#1ed760;" />
-            </svg>
+            </v-list-item-content>
+          </v-list-item>
 
-            <button>
+          <v-list-item dark>
+            <v-list-item-icon>
               <img src="@/assets/Spotify_search.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
               Search
-            </button>
-          </li>
-          <li>
-            <svg width="5" height="30">
-              <rect width="300" height="100" style="fill:#1ed760;" />
-            </svg>
-            <button>
-              <img src="@/assets/Spotify_yourlibrary.svg" />
-              Your Library
-            </button>
-          </li>
-        </ul>
-      </div>
+            </v-list-item-content>
+          </v-list-item>
 
-      <div class="playlists">
-        <div class="create-playlist">
-          <h3>PLAYLISTS</h3>
-          <button class="btn-create-playlist">
-            <img src="@/assets/Spotify_createplaylist.png" />
-            Create Playlist
-          </button>
-        </div>
-      </div>
-    </div>
+          <v-list-item dark>
+            <v-list-item-icon>
+              <img src="@/assets/Spotify_yourlibrary.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              Your Library
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-subheader>
+            <p class="text-uppercase">Playlists</p>
+          </v-subheader>
+          <v-list-item dark>
+            <v-list-item-icon>
+              <v-icon dark>mdi-plus-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              Create Playlist
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
   </div>
 </template>
 
@@ -56,85 +57,8 @@
 </script>
 
 <style scoped lang="scss">
-  button {
-    color: #fff;
-  }
   .navbar {
-    background-color: #040404;
     height: 88%;
     width: 15%;
-
-    .navbar-items {
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-      width: 100%;
-
-      .navbar-links {
-        display: flex;
-        align-content: center;
-        justify-content: space-between;
-        width: inherit;
-
-        ul {
-          padding: 0;
-          width: inherit;
-
-          li {
-            display: flex;
-            margin-top: 5%;
-            flex-direction: row;
-          }
-        }
-      }
-      .navbar-links button {
-        display: inline-flex;
-        align-items: center;
-        width: 100%;
-        margin-left: 6%;
-
-        img {
-          margin-right: 5%;
-        }
-
-        img:not(:last-child) {
-          width: 50%;
-        }
-      }
-      .create-playlist button {
-        display: inline-flex;
-        align-items: center;
-        img {
-          width: 15%;
-        }
-      }
-    }
-
-    .playlists {
-      margin-top: 15%;
-      margin-left: 5%;
-
-      display: inline-flex;
-      align-items: center;
-      width: 100%;
-      margin-left: 6%;
-
-      img {
-        margin-right: 5%;
-      }
-
-      .create-playlist h3 {
-        margin-bottom: 10%;
-        letter-spacing: 2px;
-        font-weight: lighter;
-        font-size: 0.7em;
-      }
-    }
-  }
-  .spotify-logo {
-    padding: 10% 0% 5% 10%;
-    img {
-      width: 60%;
-    }
   }
 </style>
