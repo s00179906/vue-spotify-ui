e7db1dd78c036558544b6892f714760d0df58c5c
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-12-11 03:37:05 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Spotify</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    text-align: center;
-    color: #2c3e50;
+  @import url('https://fonts.googleapis.com/css?family=Ubuntu&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Ubuntu', 'Montserrat';
+    text-decoration: none;
   }
 </style>
