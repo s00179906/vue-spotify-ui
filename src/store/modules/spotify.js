5ca24df9b2f2a4533833934b6de1e091e915267c
Ibraheem Ghaffar <S00179906@mail.itsligo.ie>
2019-12-12 22:25:03 +0000

    spotify.js --> added endpoint for getting user library tracks

--- src/store/modules/spotify.js
+++ src/store/modules/spotify.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const state = {
   user: null,
-  userLoggedIn: false
+  userLoggedIn: false,
+  userLibraryTracks: null
 };
 
 const getters = {
@@ -12,7 +13,7 @@ const getters = {
 
 const actions = {
   setToken() {
-    const access_token = window.location.hash.slice(14, 192);
+    const access_token = window.location.hash.slice(14, 300);
     const refresh_token = window.location.href.slice(-131);
 
     const tokens = {
@@ -35,12 +36,29 @@ const actions = {
     commit("setUser", user.data);
     commit("setUserLoggedIn", true);
     console.log(state.user);
+  },
+  async getUserLibraryTracks({ commit }) {
+    const { access_token } = await JSON.parse(localStorage.getItem("tokens"));
+
+    const tracks = await axios.get(
+      "https://api.spotify.com/v1/me/tracks?limit=50",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`
+        }
+      }
+    );
+
+    commit("setUserLibraryTracks", tracks.items);
+    console.log(state.userLibraryTracks);
   }
 };
 
 const mutations = {
   setUser: (state, user) => (state.user = user),
-  setUserLoggedIn: (state, value) => (state.userLoggedIn = value)
+  setUserLoggedIn: (state, value) => (state.userLoggedIn = value),
+  setUserLibraryTracks: (state, tracks) => (state.userLibraryTracks = tracks)
 };
 
 export default {
